8e074a9d59051cdc1ce6acb20ee0f54aec8f0e07
Yagmur <52479605+yaagmurss@users.noreply.github.com>
2022-04-08 08:41:54 +0300

    Add files via upload
    BinaryReaderWriter + BufferedStreams

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+
+namespace Directories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\200741\Desktop\Dosya\ymr\New folder");
+
+            foreach (FileInfo fi in dir.GetFiles())
+            {
+                try
+                {
+                    fi.Delete();
+                }
+                catch (Exception) { } // Ignore all exceptions
+            }
+
+            foreach (DirectoryInfo di in dir.GetDirectories())
+            {
+                //System.IO.File.Delete(di.FullName);
+                try
+                {
+                    di.Delete();
+                }
+                catch (Exception) { } // Ignore all exceptions
+            }
+        }
+    }
+}
